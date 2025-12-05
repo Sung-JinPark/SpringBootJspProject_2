8d57683e8e9286d31184e8339851a5629b670814
Sung-JinPark <91ssjj@gmail.com>
2025-12-05 16:28:14 +0900

    12

--- src/main/java/com/sist/web/controller/MusicController.java
+++ src/main/java/com/sist/web/controller/MusicController.java
@@ -2,13 +2,12 @@ package com.sist.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-// 화면 이동
 @Controller
 public class MusicController {
-	@GetMapping("/")
-	public String music_init()
-	{
-		return "main";
-	}
+   // 화면 이동 
+   @GetMapping("/")
+   public String music_init()
+   {
+	   return "main";
+   }
 }
